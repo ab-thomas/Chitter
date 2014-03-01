1d7f5142dd8ab6002016390cc47965c484a2b80d
Abraham Thomas <dysruptmedia@gmail.com>
2014-03-01 16:46:10 +0000

    Initial commit including rspec and cucumber

---
+++ README.md
@@ -0,0 +1,1 @@
+A twitter copy including rspec and cucumber
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chitter::Application.config.secret_key_base = '8bc3da13640f649ef58eb9a9422a46e557ae276923bb78a5621eb5f50ffd7e8de29696b0b4e2cd426f1020525e0411fa2981983665188de21b61165c50b99386'
